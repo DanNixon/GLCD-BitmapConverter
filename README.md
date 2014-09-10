a3ae1a4a6fae00547153fec3607b0f70b2a9bf40
Dan Nixon <dan@dan-nixon.com>
2014-09-10 13:51:48 +0100

    Initial code commit

---
+++ README.md
@@ -0,0 +1,6 @@
+GLCD Bitmap Converter
+---------------------
+
+A modified version of [this](http://pastebin.com/8XeazQjT) tool for converting bitmaps to C arrays for GLCDs.
+
+Kind of a work in progress but does work fine.
---
+++ converter.py
@@ -0,0 +1,113 @@
+from PIL import Image
+import argparse
+import sys
+
+
+def load_image(filename, target_width, target_height):
+    """
+    Loads an image, resized it to the target dimensions and returns it's data.
+    """
+
+    image = Image.open(filename, 'r')
+    image = image.resize((target_width, target_height), Image.NEAREST)
+    image_data = image.load()
+
+    return image.size[0], image.size[1], image_data
+
+
+def get_pixel_intensity(pixel, invert=False, max_value=255):
+    """
+    Gets the average intensity of a pixel.
+    """
+
+    intensity = pixel[0]
+    intensity += pixel[1]
+    intensity += pixel[2]
+
+    intensity /= 3
+
+    if invert:
+        return max_value - intensity
+    else:
+        return intensity
+
+
+def get_average_pixel_intensity(width, height, pixel_data, invert):
+    """
+    Gets the average intensity over all pixels.
+    """
+
+    avg_intensity = 0
+
+    for x in range(0, width):
+        for y in range(0, height):
+            avg_intensity += get_pixel_intensity(pixel_data[x, y], invert)
+
+    avg_intensity = avg_intensity / (width * height)
+
+    return avg_intensity
+
+def output_image_c_array(width, height, pixel_data, crossover, invert):
+    """
+    Outputs the data in a C bitmap array format.
+    """
+
+    print '{'
+
+    for y in range(0, height):
+        next_line = ''
+        next_value = 0
+
+        for x in range(0, width):
+            if(x % 8 == 0 and x > 0):
+                next_line += str('0x%0.2X' % next_value).lower() + ","
+                next_value = 0
+
+            if(get_pixel_intensity(pixel_data[x, y], invert) > crossover):
+                next_value += 2 ** (7 - (x % 8))
+
+        print next_line
+
+    print '};'
+
+def convert(args):
+    """
+    Runs an image conversion.
+    """
+
+    width, height, image_data = load_image(args.image, args.width, args.height)
+
+    print 'Width', width
+    print 'Height', height
+
+    crossover_intensity = get_average_pixel_intensity(width, height, image_data, args.invert)
+    output_image_c_array(width, height, image_data, crossover_intensity, args.invert)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert a bitmap image to a C array for GLCDs')
+
+    parser.add_argument(
+            '-i', '--invert',
+            action='store_true',
+            help='Invert image intensity')
+
+    parser.add_argument(
+            '--width',
+            default=64,
+            type=int,
+            help='Width of the output image')
+
+    parser.add_argument(
+            '--height',
+            default=128,
+            type=int,
+            help='Height of the output image')
+
+    parser.add_argument(
+            '-f', '--image',
+            type=str,
+            help='Image file to convert')
+
+    args = parser.parse_args()
+
+    convert(args)
